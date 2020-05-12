ef3edc62613c7df6c2a3e0d2da5a977da5bcf6f1
Shay Reichert <63343303+Kurtzs@users.noreply.github.com>
2020-05-12 10:20:45 +0200

    initial project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('.btn').mouseenter(function() {
+        $('body').css('background-color', 'black');
+        //$('h1').css('color', 'white');
+        $('.btn').css('color', 'white');
+    }),
+    $('.btn').mouseleave(function() {
+        $('body').css('background-color', '#092436');
+        //$('h1').css('color', 'black'),
+        $('.btn').css('color', 'black');
+    })
+
+});
