81cb1026f2599a7a9bcf5af435fa9f980d1e90fb
Shay Reichert <63343303+Kurtzs@users.noreply.github.com>
2020-05-13 11:44:41 +0200

    suppression js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-$(function() {
-    $('.btn').mouseenter(function() {
-        $('body').css('background-color', 'black');
-        //$('h1').css('color', 'white');
-        $('.btn').css('color', 'white');
-    }),
-    $('.btn').mouseleave(function() {
-        $('body').css('background-color', '#092436');
-        //$('h1').css('color', 'black'),
-        $('.btn').css('color', 'black');
-    })
+// $(function() {
+//     $('.btn').mouseenter(function() {
+//         $('body').css('background-color', 'black');
+//         //$('h1').css('color', 'white');
+//         $('.btn').css('color', 'white');
+//     }),
+//     $('.btn').mouseleave(function() {
+//         $('body').css('background-color', '#092436');
+//         //$('h1').css('color', 'black'),
+//         $('.btn').css('color', 'black');
+//     })
 
-});
+// });
